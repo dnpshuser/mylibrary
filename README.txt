81f7efe09d48db3c9f5204af1144a58506454107
dnpshuser <sk9632070@gmail.com>
2020-04-16 17:44:06 +0530

    Initial Setup

---
+++ README.txt
@@ -0,0 +1,2 @@
+set the environment variable NODE_ENV = development
+before running the project
---
+++ server.js
@@ -0,0 +1,47 @@
+if(process.env.NODE_ENV == 'development'){
+    require('dotenv').config();    
+}   
+    
+
+
+const express = require("express");
+const app = express();
+const indexRouter = require("./routes/index");
+const expressLayouts = require("express-ejs-layouts");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE_URL, {
+     useUnifiedTopology: true ,
+     useNewUrlParser: true
+});
+
+    const db = mongoose.connection;
+    db.on("error", error => console.error(error))
+    db.once("open", () => console.log("Connected to the Database...."));
+
+
+
+
+
+
+
+
+app.use("/", indexRouter); 
+
+
+
+
+
+
+const PORT = 3000;
+
+app.listen(PORT , () => {
+    console.log(`listening to the port ${PORT}`);
+});
