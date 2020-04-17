96b204826eca9b058b0c4351eec3a945132ebcf3
dnpshuser <sk9632070@gmail.com>
2020-04-17 05:56:10 +0530

    2nd Phase done

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 if( process.env.NODE_ENV != 'production') {
     require('dotenv').config();    
 }
-   
     
 
 
 const express = require("express");
 const app = express();
-const indexRouter = require("./routes/index");
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+
+const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+
+
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
 
 const mongoose = require("mongoose");
 
@@ -35,6 +40,7 @@ mongoose.connect(process.env.DATABASE_URL, {
 
 
 app.use("/", indexRouter); 
+app.use('/authors',authorRouter);
 
 
 
@@ -45,3 +51,4 @@ app.use("/", indexRouter);
 app.listen(process.env.PORT || 3000 , () => {
     console.log(`listening to the port ${process.env.PORT || 3000}`);
 });
+   
